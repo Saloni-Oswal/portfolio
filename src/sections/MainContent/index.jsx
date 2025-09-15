cdf3fe2616795283da128fe67310c78cdb06cdfc
Saloni Oswal <salonioswal22@gmail.com>
2025-09-15 18:27:39 -0400

    Fixed click issue for all links and hover styles due to zIndex of radial gradient element

--- src/sections/MainContent/index.jsx
+++ src/sections/MainContent/index.jsx
@@ -3,7 +3,7 @@ import { Footer } from "@/sections/Footer";
 
 export const MainContent = () => {
   return (
-    <div className='caret-transparent gap-x-4 flex basis-[0%] flex-col grow break-words gap-y-4 mx-0 md:mx-[6%]'>
+    <div className='caret-transparent gap-x-4 flex basis-[0%] flex-col grow break-words gap-y-4 mx-0 md:mx-[12%] z-10'>
       <ContentWrapper />
       <Footer />
     </div>
