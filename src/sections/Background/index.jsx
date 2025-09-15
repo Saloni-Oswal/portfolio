f1796258692dd05f6e3a7825ab148710c281f17f
Saloni Oswal <salonioswal22@gmail.com>
2025-09-15 18:28:07 -0400

    Updated styles for Background section.

--- src/sections/Background/index.jsx
+++ src/sections/Background/index.jsx
@@ -1,11 +1,11 @@
 export const Background = () => {
   return (
     <section className='caret-transparent float-none break-words w-full mb-5 md:float-right md:mb-[60px]'>
-      <h1 className='text-[19.2px] bg-teal-900 caret-transparent leading-[21.12px] break-words -ml-2.5 mb-5 p-2.5 rounded-[15px]'>
+      <h1 className='text-2xl bg-teal-900 leading-[21.12px] break-words -ml-2.5 mb-5 p-2.5 rounded-[15px]'>
         Background
       </h1>
-      <div className='caret-transparent break-words rounded-[15px]'>
-        <p className='text-slate-400 text-[14.4px] caret-transparent leading-[25.6px] break-words pt-0 pb-[15px] px-0 md:p-[25px]'>
+      <div className='break-words rounded-[15px] text-slate-400 text-base hover:bg-[rgba(45,212,191,0.1)]  hover:text-[#e2e8f0] leading-[25.6px]'>
+        <p className='pt-0 pb-[15px] px-0 md:p-[25px]'>
           In 2016, fresh off completing my Master&#39;s in Computer Science, I
           embarked on a journey to explore web development by creating a custom
           static website. Little did I know, this venture would plunge me deep
@@ -18,7 +18,7 @@ export const Background = () => {
           boasts aesthetic appeal but also operates flawlessly beneath the
           surface.
         </p>
-        <p className='text-slate-400 text-[14.4px] caret-transparent leading-[25.6px] break-words pt-0 pb-[15px] px-0 md:p-[25px]'>
+        <p className='px-0 md:p-[25px]'>
           From November 2022 I intentionally took a hiatus from my professional
           career to navigate a significant life transition. This break allowed
           me the opportunity to focus on personal growth, family commitments,
@@ -30,7 +30,7 @@ export const Background = () => {
           wealth of experiences that have shaped me both personally and
           professionally.
         </p>
-        <p className='text-slate-400 text-[14.4px] caret-transparent leading-[25.6px] break-words pt-0 pb-[15px] px-0 md:p-[25px]'>
+        <p className='pt-0 pb-[15px] px-0 md:p-[25px]'>
           When I&#39;m not immersed in code, you can often find me exploring the
           vibrant streets of NYC, camera in hand, capturing moments of life and
           architecture. I have a penchant for savoring a good cup of coffee
