c4a9bf13dbb568e34b96339c210d365039b6c285
Saloni Oswal <salonioswal22@gmail.com>
2025-09-16 12:58:23 -0400

    Image loading made proper

--- src/sections/Sidebar/components/ProfileImage.jsx
+++ src/sections/Sidebar/components/ProfileImage.jsx
@@ -1,8 +1,10 @@
+const BASE_URL = import.meta.env.BASE_URL;
+
 export const ProfileImage = () => {
   return (
     <p className='leading-[25.6px] pt-0 pb-[15px] px-0 md:p-[25px]'>
       <img
-        src='src/assets/profile.png'
+        src={`${BASE_URL}profile.png`}
         alt='Logo'
         className='h-[180px] max-w-full w-[180px] md:h-[270px] md:w-[270px]'
       />
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  publicDir: "./static",
+  publicDir: "./public",
   base: "./",
   resolve: {
     alias: {
