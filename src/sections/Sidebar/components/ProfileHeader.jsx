35ffe38b5682c71ebd6354451812dbe0fcfa4f76
Saloni Oswal <salonioswal22@gmail.com>
2025-10-06 11:33:29 -0400

    Fixed github portfolio direct link

--- src/sections/Sidebar/components/ProfileHeader.jsx
+++ src/sections/Sidebar/components/ProfileHeader.jsx
@@ -2,7 +2,7 @@ export const ProfileHeader = () => {
   return (
     <h1 className='text-2xl font-bold leading-[26.4px] mb-5 p-2.5'>
       <a
-        href='https://saloni-oswal.github.io/'
+        href='https://saloni-oswal.github.io/portfolio'
         target='_blank'
         className='hover:text-slate-50 hover:border-slate-50'
       >
